fde685f164682f124caf945bcf32da8fad152dbc
stefan <Stefan.per.larsson@gmail.com>
2021-03-31 16:10:03 +0900

    インポートやフォーマット

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\ShortLink;
+use Illuminate\Contracts\View\Factory;
 
 class IndexController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Factory
      */
-    public function index()
+    public function index(): Factory
     {
         $links = ShortLink::latest()->get();
 
